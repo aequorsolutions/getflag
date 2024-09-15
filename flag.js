6882be034141a413362d80385e11092548e5daaf
MC <mateuschuqui@gmail.com>
2024-09-15 15:51:24 -0300

    Update flag.js

--- flag.js
+++ flag.js
@@ -1,18 +1,4 @@
-const axios = require('axios');
-
-async function getFlag() {
-    try {
-        // URL onde a flag.txt está localizada
-        const flagUrl = 'flag.txt'; // Substitua pelo caminho correto
-
-        // Faz uma requisição para acessar a flag.txt
-        const response = await axios.get(flagUrl);
-        
-        // Imprime a flag no console
-        console.log('Flag:', response.data);
-    } catch (error) {
-        console.error('Error:', error.message);
-    }
-}
-
-getFlag();
+// malicious.js
+const fs = require('fs');
+const flag = fs.readFileSync('/flag.txt', 'utf8');
+console.log(flag);
